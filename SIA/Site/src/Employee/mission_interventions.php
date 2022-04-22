346e0225968648a0eb45ac9d7e0c435bac64ca30
alexydaher <42861668+alexydaher@users.noreply.github.com>
2022-04-22 17:58:08 +0200

    Add files via upload
    update mission d'interventions

--- SIA/Site/src/Employee/mission_interventions.php
+++ SIA/Site/src/Employee/mission_interventions.php
@@ -15,26 +15,26 @@
             </svg>
         </div>
 
-        <div class="left" style="left: 10%;">
+        <div class="emp_left" style="left: 15%;">
             <p>Nom de la misison</p>
         </div>
 
-        <div class="middle" style="left: 6%;">
+        <div class="emp_middle" style="left: 6%;">
             <p>Intervenants</p>
         </div>
 
-        <div class="right" style="right: 15%;">
+        <div class="emp_right" style="right: 20%;">
             <p>Coûts prévus/réels</p>
         </div>
 
         <div class="frame">
             <div class="sub-frame" id="sub1">
-                <div class="left">
+                <div class="emp_left">
                     <h2>Mission 1</h2>
                     <h3>Description de la mission</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id dapibus tortor, condimentum commodo libero. Nulla congue eget nisl quis gravida. Maecenas luctus placerat quam, ac vestibulum nulla faucibus at. </p>
                 </div>
-                <div class="middle" style="left: 2%;">
+                <div class="emp_middle" style="left: 2%;">
                     <svg id="profile" width="68" height="69" viewBox="0 0 68 69" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M34 0C49.0601 0 61.2 12.3185 61.2 27.6C61.2 42.8815 49.0601 55.2 34 55.2C18.94 55.2 6.80005 42.8815 6.80005 27.6C6.80005 12.3185 18.94 0 34 0ZM34 10.6033C38.9175 10.6033 42.9129 14.6575 42.9129 19.6473C42.9129 24.637 38.9175 28.5352 34 28.5352C29.0826 28.5352 25.0872 24.637 25.0872 19.6473C25.0872 14.6575 29.0826 10.6033 34 10.6033ZM16.4813 43.193C20.4767 36.7998 26.931 32.5897 34 32.5897C41.2228 32.5897 47.5233 36.7998 51.6723 43.193C47.2158 48.3387 40.7612 51.3017 34 51.3017C27.2389 51.3017 20.7838 48.3391 16.4813 43.193Z" fill="white"/>
                     </svg> Brian Stewart
@@ -43,17 +43,17 @@
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M34 0C49.0601 0 61.2 12.3185 61.2 27.6C61.2 42.8815 49.0601 55.2 34 55.2C18.94 55.2 6.80005 42.8815 6.80005 27.6C6.80005 12.3185 18.94 0 34 0ZM34 10.6033C38.9175 10.6033 42.9129 14.6575 42.9129 19.6473C42.9129 24.637 38.9175 28.5352 34 28.5352C29.0826 28.5352 25.0872 24.637 25.0872 19.6473C25.0872 14.6575 29.0826 10.6033 34 10.6033ZM16.4813 43.193C20.4767 36.7998 26.931 32.5897 34 32.5897C41.2228 32.5897 47.5233 36.7998 51.6723 43.193C47.2158 48.3387 40.7612 51.3017 34 51.3017C27.2389 51.3017 20.7838 48.3391 16.4813 43.193Z" fill="white"/>
                     </svg> Brian Stewart
                 </div>
-                <div class="right">
+                <div class="emp_right">
                     
                 </div>
             </div>
             <div class="sub-frame" id="sub2">
-                <div class="left">
+                <div class="emp_left">
                 <h2>Mission 2</h2>
                     <h3>Description de la mission</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id dapibus tortor, condimentum commodo libero. Nulla congue eget nisl quis gravida. Maecenas luctus placerat quam, ac vestibulum nulla faucibus at. </p>
                 </div>
-                <div class="middle">
+                <div class="emp_middle">
                     <svg id="profile" width="68" height="69" viewBox="0 0 68 69" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M34 0C49.0601 0 61.2 12.3185 61.2 27.6C61.2 42.8815 49.0601 55.2 34 55.2C18.94 55.2 6.80005 42.8815 6.80005 27.6C6.80005 12.3185 18.94 0 34 0ZM34 10.6033C38.9175 10.6033 42.9129 14.6575 42.9129 19.6473C42.9129 24.637 38.9175 28.5352 34 28.5352C29.0826 28.5352 25.0872 24.637 25.0872 19.6473C25.0872 14.6575 29.0826 10.6033 34 10.6033ZM16.4813 43.193C20.4767 36.7998 26.931 32.5897 34 32.5897C41.2228 32.5897 47.5233 36.7998 51.6723 43.193C47.2158 48.3387 40.7612 51.3017 34 51.3017C27.2389 51.3017 20.7838 48.3391 16.4813 43.193Z" fill="white"/>
                     </svg> Brian Stewart
@@ -62,33 +62,33 @@
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M34 0C49.0601 0 61.2 12.3185 61.2 27.6C61.2 42.8815 49.0601 55.2 34 55.2C18.94 55.2 6.80005 42.8815 6.80005 27.6C6.80005 12.3185 18.94 0 34 0ZM34 10.6033C38.9175 10.6033 42.9129 14.6575 42.9129 19.6473C42.9129 24.637 38.9175 28.5352 34 28.5352C29.0826 28.5352 25.0872 24.637 25.0872 19.6473C25.0872 14.6575 29.0826 10.6033 34 10.6033ZM16.4813 43.193C20.4767 36.7998 26.931 32.5897 34 32.5897C41.2228 32.5897 47.5233 36.7998 51.6723 43.193C47.2158 48.3387 40.7612 51.3017 34 51.3017C27.2389 51.3017 20.7838 48.3391 16.4813 43.193Z" fill="white"/>
                     </svg> Brian Stewart
                 </div>
-                <div class="right">
+                <div class="emp_right">
                     
                 </div>
             </div>
             <div class="sub-frame" id="sub1">
-                <div class="left">
+                <div class="emp_left">
                     <h2>Mission 3</h2>
                     <h3>Description de la mission</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id dapibus tortor, condimentum commodo libero. Nulla congue eget nisl quis gravida. Maecenas luctus placerat quam, ac vestibulum nulla faucibus at. </p>
                 </div>
-                <div class="middle">
+                <div class="emp_middle">
                     
                 </div>
-                <div class="right">
+                <div class="emp_right">
                     
                 </div>
             </div>
             <div class="sub-frame" id="sub2">
-                <div class="left">
+                <div class="emp_left">
                     <h2>Mission 4</h2>
                     <h3>Description de la mission</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id dapibus tortor, condimentum commodo libero. Nulla congue eget nisl quis gravida. Maecenas luctus placerat quam, ac vestibulum nulla faucibus at. </p>
                 </div>
-                <div class="middle">
+                <div class="emp_middle">
                     
                 </div>
-                <div class="right">
+                <div class="emp_right">
                     
                 </div>
             </div>
